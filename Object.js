384ca9859ee54cc13a1316bd9f69425419d41b03
BuatBelajar1290 <132242541+BuatBelajar1290@users.noreply.github.com>
2023-05-01 09:44:31 +0700

    first commit

---
+++ Object.js
@@ -0,0 +1,41 @@
+/**
+ * TODO
+ * 1. Buatlah variabel dengan nama restaurant yang bertipe object dengan ketentuan berikut:
+ *    - Memiliki properti bernama "name"
+ *       - Bertipe data string
+ *       - Bernilai "Bakso Mang Dicoding".
+ *    - Memiliki properti bernama "city"
+ *       - Bertipe data string
+ *       - Bernilai "Bandung".
+ *    - Memiliki properti "favorite drink"
+ *       - Bertipe data string
+ *       - Bernilai "Es Teh".
+ *    - Memiliki properti "favorite food"
+ *       - Bertipe data string
+ *       - Bernilai "Bakso".
+ *    - Memiliki properti "isVegan"
+ *       - Bertipe data boolean
+ *       - Bernilai false.
+ *
+ * 2. Buatlah variabel bernama name.
+ *    Kemudian isi dengan nilai name dari properti object restaurant
+ * 3. Buatlah variabel bernama favoriteDrink.
+ *    Kemudian isi dengan nilai "favorite drink" dari properti object restaurant
+ */
+
+// Tulis kode di bawah ini
+var restaurant = {
+	name: "Bakso Mang Dicoding",
+  	city: "Bandung",
+  	"favorite drink": "Es Teh", // supaya bisa 2 kata dan mengandung spasi
+  	"favorite food": "Bakso", // supaya bisa 2 kata dan mengandung spasi
+  	isVegan: false
+};
+
+// untuk mendapatkan nilai name dan favorite drink
+var name = restaurant.name // supaya bisa akses nilai dari property object name
+var favoriteDrink = restaurant["favorite drink"] // supaya bisa akses nilai dari property object "favorite drink" (mengandung spasi)
+
+// Test
+console.log(name);
+console.log(favoriteDrink);
